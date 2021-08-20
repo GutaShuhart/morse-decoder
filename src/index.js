b5d505a01d53b7fb5e8501bcd5ed5a759771cad2
Liza <tala.91@mail.ru>
2021-08-20 11:00:14 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let codeArray = expr.match(/.{0,10}/g);
+    codeArray = codeArray.filter(function(entry) { return entry.trim() != ''; });
+    
+   console.log(codeArray);
+   let str ='';
+    
+    let morseArr = [];
+    for( let i=0; i< codeArray.length; i++) {
+      const searchRegExp = /10/g;
+      const replaceWith = '.';
+      const searchRegExp11 = /11/g;
+      const replaceWith11 = '-';
+      const searchRegExp0 = /0/g;
+      const replaceWith0 = '';
+      MORSE_TABLE['**********']=' ';
+      const a = codeArray[i].replace(searchRegExp, replaceWith).replace(searchRegExp11, replaceWith11).replace(searchRegExp0, replaceWith0);
+
+       str += MORSE_TABLE[a];
+    }
+   return str;
+    
 }
 
 module.exports = {
